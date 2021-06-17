4af78df9b229803a56ad84c8b631495e93e2f750
Foysal Mohammad <foysal@earlblue.com>
2021-06-17 22:53:43 +0100

    created actions and added dispatcher

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import './App.css'
+import { useSelector, useDispatch } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { actionCreators } from './state/index.js'
 
 function App() {
-  return <div className="App">Hello Redux</div>
+  const account = useSelector((state) => state.account)
+  const dispatch = useDispatch()
+  const { depositMoney, withdrawMoney } = bindActionCreators(
+    actionCreators,
+    dispatch,
+  )
+
+  return (
+    <div className="App">
+      <h3>{account}</h3>
+      <button onClick={() => depositMoney(10)}>Add</button>
+      <button onClick={() => withdrawMoney(10)}>Subtract</button>
+    </div>
+  )
 }
 
 export default App

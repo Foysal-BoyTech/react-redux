520a0130e9370ef92d49ca86b179d0ce379046d3
Foysal Mohammad <foysal@earlblue.com>
2021-07-01 15:44:57 +0100

    added overlay on btn click

--- src/components/GameItem/GameItem.js
+++ src/components/GameItem/GameItem.js
@@ -8,33 +8,53 @@ import '../../sass/layout.scss'
 import styles from './GameItem.module.scss'
 
 function GameItem({ game }) {
-  const { checkList } = useContext(GameContext)
+  const { checkList, basketList } = useContext(GameContext)
+
+  function isAdded(game) {
+    for (let i = 0; i < basketList.length; i++) {
+      if (basketList[i].id === game.id) return true
+    }
+    return false
+  }
 
   if (game) {
     return (
-      <div key={game.id} className={styles['game-card']}>
-        <div className={styles['game-info']}>
-          <img src={game.image.default} className={styles['game-img']} alt="" />
-          <div className={styles['game-item']}>
-            <div>
-              <div className={styles['title-description']}>
-                <h3>{game.title}</h3>
-                <p className={styles['game-description']}>{game.description}</p>
+      <div className={styles['card-wrapper']}>
+        <div key={game.id} className={styles['game-card']}>
+          <div className={styles['game-info']}>
+            <img
+              src={game.image.default}
+              className={styles['game-img']}
+              alt=""
+            />
+            <div className={styles['game-item']}>
+              <div>
+                <div className={styles['title-description']}>
+                  <h3>{game.title}</h3>
+                  <p className={styles['game-description']}>
+                    {game.description}
+                  </p>
+                </div>
+                <p className={styles['gil']}>
+                  <img src={coins} height="15px" width="15px" alt="" />{' '}
+                  {game.gil} Gil
+                </p>
               </div>
-              <p className={styles['gil']}>
-                <img src={coins} height="15px" width="15px" alt="" /> {game.gil}{' '}
-                Gil
-              </p>
+              <button
+                className={styles['add-btn']}
+                onClick={() => checkList(game)}
+              >
+                Add to Basket
+              </button>
             </div>
-            <button
-              className={styles['add-btn']}
-              onClick={() => checkList(game)}
-            >
-              Add to Basket
-            </button>
           </div>
+          <img src={controller} className={styles['controller-img']} alt="" />
         </div>
-        <img src={controller} className={styles['controller-img']} alt="" />
+        <div
+          className={
+            isAdded(game) ? styles['overlay'] : styles['overlay-hidden']
+          }
+        ></div>
       </div>
     )
   }
